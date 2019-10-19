ca2d9c0e7de6046ae93ae242990dfcdd9136c902
Alexander Ratushnyi <forcewizu@gmail.com>
2019-10-19 13:37:44 +0300

    add initial layput

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import SectionHead from '../components/SectionHead';
 
 function App() {
   return (
     <div className="app">
-      app
+      <div className="app__container">
+        <h1>Dahsboard</h1>
+        <SectionHead/>
+      </div>
     </div>
   );
 }
---
+++ src/components/SectionHead.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SectionHead = () => {
+
+  return (
+    <div>section head</div>
+  )
+}
+
+export default SectionHead;

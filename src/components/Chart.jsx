4bed74f4ab1b79660373499443438b4af4c05c07
Alexander Ratushnyi <forcewizu@gmail.com>
2019-10-29 17:27:03 +0200

    add tradingview

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,76 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from 'react-redux';
-import ChartComponent from "react-chartjs-2";
-import 'chartjs-chart-financial';
-import "chartjs-plugin-annotation";
-import 'chartjs-plugin-zoom';
 import { getAnnotations } from '../store/getters';
 
-const Chart = ({ chart, annotations }) => {
-
-  const [dataChart, updateDataChart] = useState({
-    datasets: [{ label: "Exhcange", data: [] }]
-  })
-
-  const [options, updateOptions] = useState({
-    maintainAspectRatio: true,
-    responsive: true,
-    legend: {
-      display: false
-    },
-    scales: {
-      yAxes: [{
-        type: 'financialLinear'
-      }],
-      xAxes: [
-        {
-          display: true,
-          stacked: true,
-          beginAtZero: true,
-          type: "time",
-          time: {
-            unit: "day",
-            displayFormats: {
-              day: "DD/MM/YYYY"
-            }
-          },
-          ticks: {
-            autoSkip: true,
-            maxTicksLimit: 4.1,
-          }
-        }
-      ]
-    },
-    plugins: {
-      zoom: {
-        pan: {
-          enabled: true,
-          mode: 'xy',
-        },
-        zoom: {
-          enabled: false,
-          drag: true,
-          mode: 'xy',
-          speed: 0.1,
-        }
-      },
-    },
-    annotation: {
-      annotations: []
-    }
-  })
-
+const Chart = ({ annotations }) => {
+  
   useEffect(() => {
-    updateDataChart({ datasets: [ { ...dataChart.datasets[0], label: "Exhcange", data: [...chart] } ] });
-    // eslint-disable-next-line
-  }, [chart])
-
-  useEffect(() => {
-    updateOptions(prevState => ({ ...prevState, annotation: { annotations }}));
-  }, [annotations])
-
+    const params = {
+      "width": '100%',
+      "height": 510,
+      "symbol": "BITMEX:XBTUSD",
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "Light",
+      "style": "1",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "allow_symbol_change": true,
+      "container_id": "tradingview_5abd4",
+    };
+    new window.TradingView.widget(params);
+  }, [])
   return (
-    <ChartComponent width={102} height={50} type='candlestick' data={dataChart} options={options} />
+    <div class="tradingview-widget-container">
+      <div id="tradingview_5abd4"></div>
+    </div>
+    
   );
 };
 
